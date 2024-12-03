e93024230faf080d9bc9daf2984a772f9be2d65e
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2024-12-02 18:27:56 -0600

    changed main folder name

---
+++ django_react_quickstart/django_plaid_server/server/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('create_link_token/', views.create_link_token, name='create_link_token'),
+    path('exchange_public_token/', views.exchange_public_token, name='exchange_public_token'),
+    path('balance/', views.get_balance, name='get_balance'),
+    path('csrf_token/', views.csrf_token, name='csrf_token'),
+]
---
+++ django_react_quickstart/examples.js
@@ -0,0 +1,8 @@
+const names = {
+    '1': 'alex',
+    "2": 'adam',
+};
+
+names[3] = 'bernard';
+
+console.log(names);
