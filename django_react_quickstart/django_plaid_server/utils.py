92067c43cabc743fda78eddb3bf5267ad07f7039
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2024-12-09 18:21:10 -0600

    fixing wrapper issue

--- django_react_quickstart/django_plaid_server/utils.py
+++ django_react_quickstart/django_plaid_server/utils.py
@@ -11,6 +11,6 @@ def validate_access_token(view_func):
         if not access_token:
             return JsonResponse({'error': 'Access token not available.'}, status=403)
 
-        return view_func(request, *args, **kwargs)
+        return view_func(request, access_token, *args, **kwargs)
 
     return wrapper
