c035de9095b63c1ae0add4cb00735adfb17363a3
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2024-12-10 18:59:42 -0600

    s

--- django_react_quickstart/django_plaid_server/utils.py
+++ django_react_quickstart/django_plaid_server/utils.py
@@ -4,7 +4,7 @@ from django.http import JsonResponse
 # WRAPPER FUNCTIONS FOR VIEWS
 
 def validate_access_token(view_func):
-    """wrapper function that checks if user has a valid access token."""
+    """Wrapper function that checks if user has a valid access token."""
     @wraps(view_func)
     def wrapper(request, *args, **kwargs):
         access_token = request.session.get("access_token")
