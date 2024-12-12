1ac56044723a227b8b542dfa7745877e29c5317f
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2024-12-12 11:40:09 -0600

    misc

--- django_react_quickstart/misc.py
+++ django_react_quickstart/misc.py
@@ -1,10 +1,13 @@
-import json
+print(__name__)
 
-# Python object
-data = {"name": "Alice", "age": 30, "is_student": False}
 
-# Convert Python object to JSON string
-json_string = json.dumps(data)
-print(json_string)  # Output: '{"name": "Alice", "age": 30, "is_student": false}'
-print('\n'*2, '='*50, '\n'*2)
-print(data)
+# import json
+
+# # Python object
+# data = {"name": "Alice", "age": 30, "is_student": False}
+
+# # Convert Python object to JSON string
+# json_string = json.dumps(data)
+# print(json_string)  # Output: '{"name": "Alice", "age": 30, "is_student": false}'
+# print('\n'*2, '='*50, '\n'*2)
+# print(data)
